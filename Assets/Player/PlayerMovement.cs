ff5f6d2cb25067474c56c9d455da444bf4978305
Moedefeis <mathi-2721@outlook.dk>
2024-02-09 16:00:32 +0100

    Added ceiling check and fixed ground check

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -8,28 +8,28 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Assignables")]
     [SerializeField] Transform cam;
-    //[SerializeField] Transform groundCheck;
-    //[SerializeField] LayerMask groundMask;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
 
-    [Header("Movement Speeds")]
+    [Header("Movement Settings")]
+    [SerializeField] bool toggleRun;
     [SerializeField] public float walkSpeed;
     [SerializeField] public float runSpeed;
     [SerializeField] public float crouchSpeed;
 
     [Header("Crouch Settings")]
+    [SerializeField] bool toggleCrouch;
     [SerializeField] float crouchHeight;
     [SerializeField] float cameraCrouchOffset;
     [SerializeField] float smoothCrouchTime;
     [SerializeField] float smoothCrouchSpeed;
+    [SerializeField] float ceilingCheckDistance;
 
     [Header("Jump and Gravity")]
+    [SerializeField] bool enableJump = true;
     [SerializeField] float jumpHeight;
     [SerializeField] float gravity;
     [SerializeField] float airGravity;
-
-    [Header("Other")]
-    [SerializeField] bool toggleRun;
-    [SerializeField] bool toggleCrouch;
 
     CharacterController controller;
     Vector3 velocity;
@@ -37,8 +37,12 @@ public class PlayerMovement : MonoBehaviour
     float currentSpeed;
     float airSpeed;
     Vector3 cameraPosition;
+    Vector3 groundCheckPosition;
     float controllerHeight;
     bool isGrounded;
+    bool canStand;
+    bool ungrounded;
+    bool jumping;
 
     public bool IsWalking { get; private set; }
     public bool IsRunning { get; private set; }
@@ -47,9 +51,10 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
-        currentSpeed = walkSpeed;
+        airSpeed = currentSpeed = walkSpeed;
         cameraPosition = cam.localPosition;
         controllerHeight = controller.height;
+        groundCheckPosition = groundCheck.localPosition;
     }
 
     public void Move(InputAction.CallbackContext context) 
@@ -62,6 +67,8 @@ public class PlayerMovement : MonoBehaviour
         if (dir.y <= 0) IsRunning = false;
         currentSpeed = IsRunning ? runSpeed : (IsCrouching ? crouchSpeed : walkSpeed);
         IsWalking = dir.magnitude > 0f && !IsRunning && isGrounded;
+
+        canStand = !Physics.Raycast(transform.position, Vector3.up, ceilingCheckDistance) && IsCrouching;
 
         var move = transform.right * dir.x + transform.forward * dir.y;
         controller.Move(Time.deltaTime * ((isGrounded ? currentSpeed : airSpeed) * move + velocity));
@@ -78,15 +85,19 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (!isGrounded || !context.performed) return;
+        if (!enableJump || jumping || !isGrounded || !context.performed) return;
         StopCrouch();
+        jumping = true;
         velocity.y = Mathf.Sqrt(jumpHeight * 2f * airGravity);
         airSpeed = currentSpeed;
+        Invoke(nameof(ResetJump), 0.2f);
     }
+
+    void ResetJump() => jumping = false;
 
     public void Crouch(InputAction.CallbackContext context)
     {
-        if (toggleCrouch && !context.performed || context.started) return;
+        if (toggleCrouch && !context.performed || context.started || (IsCrouching && !canStand)) return;
         IsCrouching = isGrounded && (toggleCrouch ? !IsCrouching : context.performed);
         IsRunning = false;
         StartCoroutine(SmoothCrouch());
@@ -99,6 +110,9 @@ public class PlayerMovement : MonoBehaviour
         var cameraStart = cam.localPosition;
         var cameraEnd = IsCrouching ? cameraPosition + new Vector3(0f, cameraCrouchOffset, 0f) : cameraPosition;
 
+        var groundStart = groundCheck.localPosition;
+        var groundEnd = IsCrouching ? groundCheckPosition + new Vector3(0f, (controllerHeight - crouchHeight) / 2f, 0f) : groundCheckPosition;
+
         var heightStart = controller.height;
         var heightEnd = IsCrouching ? crouchHeight : controllerHeight;
 
@@ -106,6 +120,7 @@ public class PlayerMovement : MonoBehaviour
         {
             time += Time.deltaTime * smoothCrouchSpeed;
             cam.localPosition = Vector3.Lerp(cameraStart, cameraEnd, time);
+            groundCheck.localPosition = Vector3.Lerp(groundStart, groundEnd, time);
             controller.height = Mathf.Lerp(heightStart, heightEnd, time);
             yield return null;
         }
@@ -113,17 +128,20 @@ public class PlayerMovement : MonoBehaviour
 
     void StopCrouch()
     {
+        if (IsCrouching && !canStand) return;
         IsCrouching = false;
         StartCoroutine(SmoothCrouch());
     }
 
     void Gravity()
     {
-        isGrounded = controller.isGrounded;// Physics.CheckSphere(groundCheck.position, 0.4f, groundMask);
-        if (isGrounded && velocity.y < 0)
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.4f, groundMask);
+        if ((isGrounded || !isGrounded && !ungrounded) && velocity.y < 0)
         {
             velocity.y = -2f;
+            ungrounded = !isGrounded;
         }
-        velocity.y -= (isGrounded ? gravity : airGravity) * Time.deltaTime;
+
+        velocity.y -= (isGrounded && !jumping ? gravity : airGravity) * Time.deltaTime;
     }
 }
