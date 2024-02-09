b5e4d03e0e6ade68a502a3f65ed42b7612c6175d
Adrian Kari <adrka@itu.dk>
2024-02-09 12:00:59 +0100

    Added Fail Percentage To All Lights

--- Assets/Scripts/LightFlicker.cs
+++ Assets/Scripts/LightFlicker.cs
@@ -16,6 +16,7 @@ public class LightFlicker : MonoBehaviour
     [SerializeField] float offFlickerSpeedMax = 0.4f;
     [SerializeField] float onFlickerSpeedMin = 0.05f;
     [SerializeField] float onFlickerSpeedMax = 2f;
+    [SerializeField] int failPercentage = 20;
 
     private void Start()
     {
@@ -30,7 +31,26 @@ public class LightFlicker : MonoBehaviour
             lightIntensities[i] = childObjects[i].GetComponent<Light>().intensity;
         }
 
-        StartCoroutine(Flicker());
+        bool failed = Random.Range(0, 100) < failPercentage;
+
+        if (!failed)
+        {
+            StartCoroutine(Flicker());
+        }
+        else
+        {
+            TurnOff();
+        }
+    }
+
+    private void TurnOff()
+    {
+        Debug.Log("Turned of light");
+        for (int i = 0; i < childObjects.Length; i++)
+        {
+            childObjects[i].GetComponent<Light>().intensity = 0;
+            glowMaterial.DisableKeyword("_EMISSION");
+        }
     }
 
     IEnumerator Flicker()
