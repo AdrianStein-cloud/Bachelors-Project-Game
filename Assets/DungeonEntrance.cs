8ed3215eee714bfb6b929365d826accd838c9993
Moedefeis <mathi-2721@outlook.dk>
2024-02-23 17:06:42 +0100

    Added elevator door open

--- Assets/DungeonEntrance.cs
+++ Assets/DungeonEntrance.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 
 public class DungeonEntrance : Interactable
 {
-    [SerializeField] GameObject ReadyLamp;
-    [SerializeField] GameObject NotReadyLamp;
-
     public bool DungeonIsAvailable { get; set; } = false;
     public Action EnterDungeon { get; set; }
     bool inFocus = false;
-
-    private bool lightsOn = true;
 
     private void Update()
     {
@@ -27,23 +22,8 @@ public class DungeonEntrance : Interactable
                 Debug.LogWarning("Display dungeon not yet available");
             }
         }
-        if(DungeonIsAvailable && !lightsOn)
-        {
-            ReadyLamp.GetComponentInChildren<Light>().intensity = 20f;
-            ReadyLamp.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
-            NotReadyLamp.GetComponentInChildren<Light>().intensity = 0f;
-            NotReadyLamp.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
-            lightsOn = true;
-        }
-        else if(!DungeonIsAvailable && lightsOn)
-        {
-            ReadyLamp.GetComponentInChildren<Light>().intensity = 0f;
-            ReadyLamp.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
-            NotReadyLamp.GetComponentInChildren<Light>().intensity = 20f;
-            NotReadyLamp.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
-            lightsOn = false;
-        }
     }
+
     public override void EnableInteractability()
     {
         inFocus = true;
---
+++ Assets/Scripts/ElevatorEntrance.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorEntrance : MonoBehaviour
+{
+    [SerializeField] GameObject ReadyLamp;
+    [SerializeField] GameObject NotReadyLamp;
+
+    private bool lightsOn = true;
+
+    DungeonEntrance entrance;
+    Animator anim;
+
+    private void Start()
+    {
+        entrance = FindObjectOfType<DungeonEntrance>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (entrance.DungeonIsAvailable && !lightsOn)
+        {
+            ReadyLamp.GetComponentInChildren<Light>().intensity = 20f;
+            ReadyLamp.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+            NotReadyLamp.GetComponentInChildren<Light>().intensity = 0f;
+            NotReadyLamp.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+            lightsOn = true;
+            anim.SetTrigger("Open");
+        }
+        else if (!entrance.DungeonIsAvailable && lightsOn)
+        {
+            ReadyLamp.GetComponentInChildren<Light>().intensity = 0f;
+            ReadyLamp.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+            NotReadyLamp.GetComponentInChildren<Light>().intensity = 20f;
+            NotReadyLamp.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+            lightsOn = false;
+        }
+    }
+}
