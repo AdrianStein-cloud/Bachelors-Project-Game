98a4b7d923f34fb7e1725bdf63c67fba92861976
Adrian Kari <adrka@itu.dk>
2024-02-27 16:20:43 +0100

    New Monster Every 3rd Round

--- Assets/Scripts/DangerScaler.cs
+++ Assets/Scripts/DangerScaler.cs
@@ -15,7 +15,7 @@ public class DangerScaler
         GameSettings.Instance.Wave++;
         GameSettings.Instance.CurrentDepth = GameSettings.Instance.DungeonStartDepth + GameSettings.Instance.Wave / 2;
         GameSettings.Instance.LightFailPercentage = (int) (85f / (1f + Mathf.Exp(-0.25f * (GameSettings.Instance.Wave - 10f))) + 10f);
-        GameSettings.Instance.EnemyAmount = (int) MathF.Floor(1 + GameSettings.Instance.Wave / 5);
+        GameSettings.Instance.EnemyAmount = (int) MathF.Floor(1 + GameSettings.Instance.Wave / 3);
     }
 
     public void RestartDanger()
