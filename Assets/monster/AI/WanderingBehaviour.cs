67fe3b28b9602cd095a15e611d0db01aa3b18865
Daniel <danielmichael.fabricius@gmail.com>
2024-02-29 16:59:54 +0100

    Added different sounds for running and walking

--- Assets/monster/AI/WanderingBehaviour.cs
+++ Assets/monster/AI/WanderingBehaviour.cs
@@ -21,9 +21,12 @@ public class WanderingBehaviour : MonoBehaviour
     private AudioSource monsterSource, footstepSource, attackSource;
     public GameObject footstepSourceGO, attackSourceGO;
 
-    public List<AudioClip> footstepSounds;
     public List<AudioClip> screamSounds;
+
+    public List<AudioClip> footstepSounds;
+    public List<AudioClip> runningFootstepSounds;
     private List<AudioClip> tempFootsteps = new List<AudioClip>();
+    private List<AudioClip> tempRunningFootsteps = new List<AudioClip>();
 
     public float lastAttackTime;
     public float attackDelay;
@@ -67,8 +70,20 @@ public class WanderingBehaviour : MonoBehaviour
 
         var clip = tempFootsteps[Random.Range(0, tempFootsteps.Count)];
         footstepSource.clip = clip;
+        footstepSource.pitch = 0.6f;
         footstepSource.PlayOneShot(clip);
         tempFootsteps.Remove(clip);
+    }
+
+    public void PlayRunningFootstepSound()
+    {
+        if (tempRunningFootsteps.Count <= 0) tempRunningFootsteps = new List<AudioClip>(runningFootstepSounds);
+
+        var clip = tempRunningFootsteps[Random.Range(0, tempRunningFootsteps.Count)];
+        footstepSource.clip = clip;
+        footstepSource.pitch = 1;
+        footstepSource.PlayOneShot(clip);
+        tempRunningFootsteps.Remove(clip);
     }
 
     public void PlayScreechSound()
