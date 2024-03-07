ee059335dc0a8a3bfb90c9d94b70164427fbd6c0
lukas175 <lukasersej175@gmail.com>
2024-03-07 14:14:30 +0100

    fixed enemy roam

--- Assets/monster/AI/StateMachine/WandererSearchRoom.cs
+++ Assets/monster/AI/StateMachine/WandererSearchRoom.cs
@@ -1,12 +1,38 @@
+using BBUnity.Actions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WandererSearchRoom : StateProcess<WandererState>
 {
+    WandererInfo info;
+    WandererMovement movement;
+
+    public float speed;
+
+    private void Awake()
+    {
+        movement = GetComponent<WandererMovement>();
+        info = GetComponent<WandererInfo>();
+    }
+
     private void OnEnable()
     {
         Debug.Log("Serach Room");
-        StateController.SwitchState(WandererState.Roam);
+
+        var roomScript = info.CurrentRoom.GetComponent<Room>();
+        var roamPoint = roomScript.roamPositions[Random.Range(0, roomScript.roamPositions.Count)];
+        movement.MoveTo(roamPoint.transform.position, speed, DestinationReached);
+    }
+
+    void DestinationReached()
+    {
+        //Debug.Log("Destination Reached");
+
+        //Only transition if roaming didn't get interrupted (currently it cant get interrupted, but just in case)
+        if (enabled)
+        {
+            StateController.SwitchState(WandererState.Roam);
+        }
     }
 }
