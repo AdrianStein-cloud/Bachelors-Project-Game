8b266054cc7773c582c2c3e9ecf3ad0e3a02def3
Adrian Kari <adrka@itu.dk>
2024-03-19 17:23:25 +0100

    No Light Drama If Poweroutage

--- Assets/Scripts/DramaMaker.cs
+++ Assets/Scripts/DramaMaker.cs
@@ -26,10 +26,14 @@ public class DramaMaker : Interactable
 
         dramaList = new List<WeightedDrama>
         {
-            new WeightedDrama(TurnOffLights, 100, 20, "Warning!\nAre you sure you want to turn off the power?\n(bonus +20$)"),
             new WeightedDrama(ReleaseMonster, 100, 50, "Warning!\nAre you sure you want to release another monter?\n(bonus +50$)"),
             new WeightedDrama(Nothing, 100, 0, "Warning!\nYou're dead..."),
         };
+
+        if (!GameSettings.Instance.PowerOutage)
+        {
+            dramaList.Add(new WeightedDrama(TurnOffLights, 100, 20, "Warning!\nAre you sure you want to turn off the power?\n(bonus +20$)"));
+        }
 
         drama = dramaList.GetRollFromWeights(random);
 
