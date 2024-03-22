d99b415769cecbcc9028862d51d69e17cae09515
Moedefeis <mathi-2721@outlook.dk>
2024-03-22 09:39:20 +0100

    Added read only attribute

---
+++ Assets/Scripts/ReadOnlyAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Read Only attribute.
+/// Attribute is use only to mark ReadOnly properties.
+/// </summary>
+[AttributeUsage(AttributeTargets.All)]
+public class ReadOnlyAttribute : PropertyAttribute { }
