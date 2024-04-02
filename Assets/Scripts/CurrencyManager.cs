31272adf31ca2877347f514dde8d78613e16e677
lukas175 <lukasersej175@gmail.com>
2024-04-02 11:49:54 +0200

    added wanderer fast sounds

--- Assets/Scripts/CurrencyManager.cs
+++ Assets/Scripts/CurrencyManager.cs
@@ -26,6 +26,7 @@ public class CurrencyManager : MonoBehaviour
     {
         UnitySingleton<CurrencyManager>.BecomeSingleton(this);
         currencyText = GameObject.Find("Currency").GetComponent<TextMeshProUGUI>();
+        currencyText.text = $"{currentCurrency} $";
     }
 
 
