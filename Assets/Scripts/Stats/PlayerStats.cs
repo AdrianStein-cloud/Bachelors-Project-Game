5062e8b73c3227b289a7e82322a6b58de1573d1d
Adrian Kari <adrka@itu.dk>
2024-04-05 10:44:57 +0200

    Added More Intuitive Key System

--- Assets/Scripts/Stats/PlayerStats.cs
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -5,4 +5,19 @@ public class PlayerStats
 {
     public int keysHeld = 0;
     public float speedMultiplier = 1f;
+
+    public void AddKey()
+    {
+        keysHeld++;
+        InventoryUI.Instance.SetKeyImageActive(true);
+    }
+
+    public void RemoveKey()
+    {
+        keysHeld--;
+        if(keysHeld <= 0)
+        {
+            InventoryUI.Instance.SetKeyImageActive(false);
+        }
+    }
 }
